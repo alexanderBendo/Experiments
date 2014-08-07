3e3a69e17eb5ab3b84216304c9d6884cb1d7df66
Alexander Bendo <alexanderbendo@directorioc.net>
2014-08-07 20:39:39 +0200

    Fix feature selection in exp006

--- exp006/classify-football-players-with-kNN.R
+++ exp006/classify-football-players-with-kNN.R
@@ -10,7 +10,7 @@ shuffled_player_data <- player_data[sample(nrow(player_data)),]
 
 # Just for convenience, drop non-numeric features as well as all kind of id features, ranking and so on
 # BUT store the target feature in the last column
-shuffled_player_data <- shuffled_player_data[,c(seq(2,8),10,11, seq(13,17),19,22,seq(24,26),34,seq(36,40),seq(44,51),seq(53,56),31)]
+shuffled_player_data <- shuffled_player_data[,c(seq(1,8),10,11, seq(13,17),19,22,seq(24,26),34,seq(36,40),seq(44,51),seq(53,56),31)]
 
 # Standardize player features
 players_z <- as.data.frame(scale(shuffled_player_data[,1:37]))
@@ -21,8 +21,8 @@ players_train <- players_z[1:1500,]
 players_test <- players_z[1501:1800,]
 
 # Store the target class label in factor vectors for evaluating model performance
-players_train_labels <- shuffled_player_data[1:1500,38]
-players_test_labels <- shuffled_player_data[1501:1800,38]
+players_train_labels <- shuffled_player_data[1:1500,39]
+players_test_labels <- shuffled_player_data[1501:1800,39]
 
 # Classify test data and evaluate model performance using different values of k
 # k=9 seems to work best, with 88.7% of correct predictions
